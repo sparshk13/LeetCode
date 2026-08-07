c7363944a615610cdbed7c78f73ea9091f6d293c
Sparsh Kumat <sparsh.k.work@outlook.com>
2026-08-08 02:46:07 +0530

    Time: 79 ms (5.53%), Space: 37.5 MB (5.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,17 +3,24 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
     
-        unordered_map<string, vector<string>> sorted;
+        unordered_map<string, vector<string>> map;
         
         for (auto str: strs) {
-            string original = str;
-            sort(str.begin(), str.end());
-            sorted[str].push_back(original);
+            vector<int> count(26, 0);
+            
+            for (auto ch: str) {
+                count[ch-'a']++;
+            }
+            string key;
+            
+            for (auto num: count) {
+                key += to_string(num) + '#';
+            }
+            map[key].push_back(str);
         }
-        for (auto it: sorted) {
+        for (auto it: map) {
             ans.push_back(it.second);
         }
-        
         return ans;
     }
 };
