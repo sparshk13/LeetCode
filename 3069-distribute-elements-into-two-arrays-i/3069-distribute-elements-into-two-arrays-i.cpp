67ab8ed0ecbbbe241d89ee65a1781be622690adb
Sparsh Kumat <sparsh.k.work@outlook.com>
2026-08-21 00:34:32 +0530

    Time: 0 ms (100%), Space: 24.3 MB (5.61%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1, arr2, result;
+        int n = nums.size();
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            }
+            else {
+                arr2.push_back(nums[i]);
+            }
+        }
+        result.insert(result.end(), arr1.begin(), arr1.end());
+        result.insert(result.end(), arr2.begin(), arr2.end());
+
+        return result;
+    }
+};
