92dd0db36a82fd4249fb6b851a7dd54c05189f45
Sparsh Kumat <sparsh.k.work@outlook.com>
2026-08-30 23:22:20 +0530

    Time: 0 ms (100%), Space: 14.3 MB (44.46%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st(nums1.begin(), nums1.end());
+        vector<int> ans;
+
+        for (auto num: nums2) {
+            if (st.find(num) != st.end()) {
+                ans.push_back(num);
+                st.erase(num);
+            }
+        }
+        return ans;
+    }
+};
