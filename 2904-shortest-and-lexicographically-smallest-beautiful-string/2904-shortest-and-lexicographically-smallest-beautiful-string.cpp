8a30ee14d525564230cab0eb9bd5c7d18cb252a7
Sparsh Kumat <sparsh.k.work@outlook.com>
2026-08-30 13:57:18 +0530

    Time: 2 ms (38.7%), Space: 9.2 MB (29.43%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
+        vector<int> index;
         int l = 0;
         int count = 0;
         string ans;
 
-        for (int r = 0; r < n; r++) {
-            if (s[r] == '1')  count++;
-
-            while (count > k) {
-                if (s[l] == '1')  count--;
-                l++;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                index.push_back(i);
             }
-            if (count == k) {
-                while (l < r && s[l] == '0')  l++;
+        }
 
-                string curr = s.substr(l, r-l+1);
+        if (index.size() < k) {
+            return ans;
+        }
 
-                if (ans == "" ||
-                    curr.size() < ans.size() ||
-                    (curr.size() == ans.size() && curr < ans)) {
-                        ans = curr;
-                    }
-            }
+        for (int i = 0; i+k-1 < index.size(); i++) {
+            int st = index[i];
+            int e = index[i+k-1];
+
+            string curr = s.substr(st, e-st+1);
+
+            if (ans == "" ||
+                curr.size() < ans.size() ||
+                (curr.size() == ans.size() && curr < ans)) {
+                    ans = curr;
+                }
         }
         return ans;
     }
