0b84c724b02bfe5dce580590ce4434ed5b403963
Sparsh Kumat <sparsh.k.work@outlook.com>
2026-09-02 08:31:36 +0530

    Time: 83 ms (47.98%), Space: 89.1 MB (24.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,6 +5,10 @@ public:
         unordered_set<int> st(nums.begin(), nums.end());
         int ans = 0;
 
+        if (nums.empty()) {
+            return 0;
+        }
+
         for (auto num: st) {
             if (st.find(num - 1) == st.end()) {
                 int temp = num;
